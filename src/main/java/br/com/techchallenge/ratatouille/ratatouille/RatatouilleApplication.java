9442d8e0e5392042911a381bad11cbb85fd8d601
CipherCrow <jonastorquatro@gmail.com>
2024-12-04 05:06:17 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/techchallenge/ratatouille/ratatouille/RatatouilleApplication.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge.ratatouille.ratatouille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatatouilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatatouilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/techchallenge/ratatouille/ratatouille/RatatouilleApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.techchallenge.ratatouille.ratatouille;
+
+import org.assertj.core.api.Fail;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatatouilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Fail.fail ("Não Implementado Ainda");
+	}
+
+}
