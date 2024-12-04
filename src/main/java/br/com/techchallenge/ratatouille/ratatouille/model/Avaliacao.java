1a680a5a7bc57744b67987c808288106ef75a426
CipherCrow <jonastorquatro@gmail.com>
2024-12-04 06:16:48 -0300

    Criando Classes model

---
+++ src/main/java/br/com/techchallenge/ratatouille/ratatouille/model/Avaliacao.java
@@ -0,0 +1,22 @@
+package br.com.techchallenge.ratatouille.ratatouille.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Avaliacao {
+    @Id
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "idRestaurante", referencedColumnName = "idRestaurante")
+    private Restaurante restaurante;
+
+    private String comentario;
+    private int estrelas;
+
+    @OneToOne
+    @JoinColumn(name = "idUsuario", referencedColumnName = "idUsuario")
+    private Usuario usuario;
+}
