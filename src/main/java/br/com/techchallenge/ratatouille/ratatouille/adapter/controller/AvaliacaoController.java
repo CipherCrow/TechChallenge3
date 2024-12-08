ec7a21d39b8c105813505ffcb3d8af9be51c5966
CipherCrow <jonastorquatro@gmail.com>
2024-12-08 16:49:23 -0300

    Controller de avaliacao

---
+++ src/main/java/br/com/techchallenge/ratatouille/ratatouille/adapter/controller/AvaliacaoController.java
@@ -0,0 +1,64 @@
+package br.com.techchallenge.ratatouille.ratatouille.adapter.controller;
+
+import br.com.techchallenge.ratatouille.ratatouille.adapter.dto.AvaliacaoDTO;
+import br.com.techchallenge.ratatouille.ratatouille.adapter.exceptions.IdJaExistenteException;
+import br.com.techchallenge.ratatouille.ratatouille.adapter.exceptions.RegistroNotFoundException;
+import br.com.techchallenge.ratatouille.ratatouille.adapter.mapper.AvaliacaoMapper;
+import br.com.techchallenge.ratatouille.ratatouille.domain.model.entities.Avaliacao;
+import br.com.techchallenge.ratatouille.ratatouille.domain.model.service.AvaliacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/avaliacao")
+public class AvaliacaoController {
+
+    @Autowired
+    AvaliacaoService avaliacaoService;
+
+    @PostMapping("/avaliar")
+    public ResponseEntity<Object> avaliarRestaurante(@RequestParam Long idRestaurante,
+                                                     @RequestParam Long idUsuario,
+                                                     @RequestBody AvaliacaoDTO avaliacaoDTO) {
+        try{
+            Avaliacao avaliacaoCriada = avaliacaoService.criar(idRestaurante,
+                                                                idUsuario,
+                                                                avaliacaoDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).
+                    body(AvaliacaoMapper.toDTO(avaliacaoCriada));
+        }catch(IdJaExistenteException | RegistroNotFoundException | NullPointerException e ){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Object> buscarUsuario(@PathVariable Long id) {
+        try{
+            Avaliacao avaliacaoEncontrada = avaliacaoService.buscarPeloId(id);
+            return ResponseEntity.status(HttpStatus.FOUND).body(AvaliacaoMapper.toDTO(avaliacaoEncontrada));
+        }catch(NullPointerException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }catch(RegistroNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/buscarAvaliacoesDoRestaurante/{id}")
+    public ResponseEntity<Object> buscarAvaliacoesDoRestaurante(@PathVariable Long id) {
+        try{
+            List<Avaliacao> avaliacoesEncontradas = avaliacaoService.buscarTodasAvaliacoesRestaurante(id);
+            return ResponseEntity.status(HttpStatus.FOUND).body(avaliacoesEncontradas);
+        }catch(NullPointerException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }catch(RegistroNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+
+}
--- src/main/java/br/com/techchallenge/ratatouille/ratatouille/domain/model/service/AvaliacaoService.java
+++ src/main/java/br/com/techchallenge/ratatouille/ratatouille/domain/model/service/AvaliacaoService.java
@@ -4,6 +4,8 @@ import br.com.techchallenge.ratatouille.ratatouille.adapter.exceptions.IdJaExist
 import br.com.techchallenge.ratatouille.ratatouille.adapter.exceptions.RegistroNotFoundException;
 import br.com.techchallenge.ratatouille.ratatouille.domain.model.entities.Avaliacao;
 import br.com.techchallenge.ratatouille.ratatouille.adapter.dto.AvaliacaoDTO;
+import br.com.techchallenge.ratatouille.ratatouille.domain.model.entities.Restaurante;
+import br.com.techchallenge.ratatouille.ratatouille.domain.model.entities.Usuario;
 import br.com.techchallenge.ratatouille.ratatouille.infrastructure.persistence.repository.AvaliacaoRepository;
 import br.com.techchallenge.ratatouille.ratatouille.adapter.mapper.AvaliacaoMapper;
 import org.slf4j.Logger;
@@ -11,27 +13,38 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
 public class AvaliacaoService {
 
-    private static final Logger log = LoggerFactory.getLogger(AvaliacaoService.class);
     private static String idNotNull = "ID não pode ser nulo";
 
     @Autowired
     private AvaliacaoRepository avaliacaoRepository;
 
-    public Avaliacao criar(AvaliacaoDTO avaliacaoDTO) {
-        log.info("Criando Avaliacao ID: {}",avaliacaoDTO.idAvaliacao());
-        Long parametroID = avaliacaoDTO.idAvaliacao();
+    @Autowired
+    private RestauranteService restauranteService;
 
-        if (avaliacaoRepository.existsById(parametroID)) {
-            log.info("ID ja existe. ID: {}", parametroID);
+    @Autowired
+    private UsuarioService usuarioService;
+
+    public Avaliacao criar(Long idRestaurante, Long idUsuario, AvaliacaoDTO avaliacaoDTO) {
+        Objects.requireNonNull(idRestaurante);
+        Objects.requireNonNull(idUsuario);
+        Objects.requireNonNull(avaliacaoDTO);
+
+        if (avaliacaoRepository.existsById(avaliacaoDTO.idAvaliacao())) {
             throw new IdJaExistenteException("Id da avaliacao já existente!");
         }
 
+        Restaurante restaurante = restauranteService.buscarPeloId(idRestaurante);
+        Usuario usuario = usuarioService.buscarPeloId(idUsuario);
+
         Avaliacao avaliacao = AvaliacaoMapper.toEntity(avaliacaoDTO);
+        avaliacao.setRestaurante(restaurante);
+        avaliacao.setUsuario(usuario);
         return avaliacaoRepository.save(avaliacao);
     }
 
@@ -40,6 +53,13 @@ public class AvaliacaoService {
 
         return avaliacaoRepository.findById(idAvaliacao)
                 .orElseThrow(() -> new RegistroNotFoundException("Avaliacao",idAvaliacao));
+    }
+
+    public List<Avaliacao> buscarTodasAvaliacoesRestaurante(Long idRestaurante) {
+        Objects.requireNonNull(idRestaurante, idNotNull);
+
+        Restaurante restaurante = restauranteService.buscarPeloId(idRestaurante);
+        return avaliacaoRepository.findAvaliacaosByRestaurante(restaurante);
     }
 
 }
