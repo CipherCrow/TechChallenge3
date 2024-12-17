3f8413cd2cdb40c2a3e82e2e137617ef6ff2b003
CipherCrow <jonastorquatro@gmail.com>
2024-12-17 00:45:22 -0300

    finalizando teste unitario da restaurante controller

--- src/test/java/br/com/techchallenge/ratatouille/adapter/controller/RestauranteControllerTest.java
+++ src/test/java/br/com/techchallenge/ratatouille/adapter/controller/RestauranteControllerTest.java
@@ -1,11 +1,14 @@
 package br.com.techchallenge.ratatouille.adapter.controller;
 
 import br.com.techchallenge.ratatouille.ratatouille.adapter.controller.RestauranteController;
+import br.com.techchallenge.ratatouille.ratatouille.adapter.dto.HorarioDTO;
 import br.com.techchallenge.ratatouille.ratatouille.adapter.dto.RestauranteDTO;
 import br.com.techchallenge.ratatouille.ratatouille.adapter.exceptions.IdJaExistenteException;
 import br.com.techchallenge.ratatouille.ratatouille.adapter.exceptions.RegistroNotFoundException;
+import br.com.techchallenge.ratatouille.ratatouille.adapter.mapper.HorarioMapper;
 import br.com.techchallenge.ratatouille.ratatouille.adapter.mapper.LocalizacaoMapper;
 import br.com.techchallenge.ratatouille.ratatouille.adapter.mapper.RestauranteMapper;
+import br.com.techchallenge.ratatouille.ratatouille.domain.model.entities.Horario;
 import br.com.techchallenge.ratatouille.ratatouille.domain.model.entities.Localizacao;
 import br.com.techchallenge.ratatouille.ratatouille.domain.model.entities.Restaurante;
 import br.com.techchallenge.ratatouille.ratatouille.domain.model.enums.TipoDeCozinhaEnum;
@@ -20,14 +23,16 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -61,7 +66,7 @@ class RestauranteControllerTest {
 
             when(restauranteService.criar(any(Restaurante.class))).thenReturn(restaurante);
 
-            mockMvc.perform(MockMvcRequestBuilders.post("/restaurante/cadastrarRestaurante")
+            mockMvc.perform(post("/restaurante/cadastrarRestaurante")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(asJsonString(restauranteDTO)))
                     .andExpect(status().isCreated())
@@ -76,7 +81,7 @@ class RestauranteControllerTest {
 
             when(restauranteService.criar(any(Restaurante.class))).thenThrow(new IdJaExistenteException("Id do restaurante já existente!"));
 
-            mockMvc.perform(MockMvcRequestBuilders.post("/restaurante/cadastrarRestaurante")
+            mockMvc.perform(post("/restaurante/cadastrarRestaurante")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(asJsonString(restauranteDTO)))
                     .andExpect(status().isBadRequest())
@@ -93,7 +98,7 @@ class RestauranteControllerTest {
 
             when(restauranteService.buscarPeloId(eq(1L))).thenReturn(restaurante);
 
-            mockMvc.perform(MockMvcRequestBuilders.get("/restaurante/buscarRestaurante/1"))
+            mockMvc.perform(get("/restaurante/buscarRestaurante/1"))
                     .andExpect(status().isFound())
                     .andExpect(jsonPath("$.nome").value("Restaurante A"));
 
@@ -104,7 +109,7 @@ class RestauranteControllerTest {
         void deveRetornarErroSeNaoEncontrado() throws Exception {
             when(restauranteService.buscarPeloId(eq(1L))).thenThrow(new RegistroNotFoundException("Restaurante", 1L));
 
-            mockMvc.perform(MockMvcRequestBuilders.get("/restaurante/buscarRestaurante/1"))
+            mockMvc.perform(get("/restaurante/buscarRestaurante/1"))
                     .andExpect(status().isNotFound())
                     .andExpect(content().string("Restaurante não encontrado com ID: 1"));
         }
@@ -116,7 +121,7 @@ class RestauranteControllerTest {
 
             when(restauranteService.buscarPeloNome(eq("Restaurante A"))).thenReturn(restaurantes);
 
-            mockMvc.perform(MockMvcRequestBuilders.get("/restaurante/buscarRestaurantePorNome/Restaurante A"))
+            mockMvc.perform(get("/restaurante/buscarRestaurantePorNome/Restaurante A"))
                     .andExpect(status().isFound())
                     .andExpect(jsonPath("$[0].nome").value("Restaurante A"));
 
@@ -127,7 +132,7 @@ class RestauranteControllerTest {
         void deveRetornarErroSeNenhumRestauranteEncontrado() throws Exception {
             when(restauranteService.buscarPeloNome(eq("Restaurante A"))).thenThrow(new RegistroNotFoundException("Restaurante", 0L));
 
-            mockMvc.perform(MockMvcRequestBuilders.get("/restaurante/buscarRestaurantePorNome/Restaurante A"))
+            mockMvc.perform(get("/restaurante/buscarRestaurantePorNome/Restaurante A"))
                     .andExpect(status().isNotFound())
                     .andExpect(content().string("Restaurante não encontrado com ID: 0"));
         }
@@ -205,6 +210,146 @@ class RestauranteControllerTest {
 
     @Nested
     class manipularHorarioEReservas{
+
+        @Test
+        void deveAdicionarHorarioComSucesso() throws Exception {
+
+            fail("Não foi possível implementar devido aos problemas com a biblioteca de time do java 8.");
+            Horario horario = new Horario();
+            horario.setIdHorario(1L);
+            horario.setHoraInicio(LocalTime.of(9, 0));
+            horario.setHoraFim(LocalTime.of(17, 0));
+            horario.setData(LocalDate.now());
+            horario.setEspacosParaReserva(10);
+            horario.setQtdReservados(0);
+            horario.setRestaurante(new Restaurante());
+
+            when(horarioService.adicionarHorarioDeFuncionamentoAoRestaurante(1L, horario))
+                    .thenReturn(horario);
+
+            mockMvc.perform(post("/restaurante/adicionarHorario/1")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(asJsonString(horario)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.horaInicio[0]").value(9))
+                    .andExpect(jsonPath("$.horaInicio[1]").value(0));
+            verify(horarioService).adicionarHorarioDeFuncionamentoAoRestaurante(1L, horario);
+        }
+
+        @Test
+        void deveRetornarErro_IdJaExistente() throws Exception {
+            fail("Não foi possível implementar devido aos problemas com a biblioteca de time do java 8.");
+
+            Horario horario = new Horario();
+            horario.setIdHorario(1L);
+            horario.setHoraInicio(LocalTime.of(9, 0));
+            horario.setHoraFim(LocalTime.of(17, 0));
+            horario.setData(LocalDate.now());
+            horario.setEspacosParaReserva(10);
+            horario.setQtdReservados(0);
+            horario.setRestaurante(new Restaurante());
+
+            HorarioDTO horarioDTO;
+            horarioDTO = HorarioMapper.toDTO(horario);
+
+            when(horarioService.adicionarHorarioDeFuncionamentoAoRestaurante(1L, horario))
+                    .thenThrow(new IdJaExistenteException("Id do Horario já existente!"));
+
+            mockMvc.perform(post("/restaurante/adicionarHorario/1")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(asJsonString(horarioDTO)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string("Id do Horario já existente!"));
+        }
+
+        @Test
+        void deveBuscarHorarioPorIdComSucesso() throws Exception {
+            Horario horario = new Horario();
+            horario.setIdHorario(1L);
+            horario.setHoraInicio(LocalTime.of(9, 0));
+            horario.setHoraFim(LocalTime.of(17, 0));
+            horario.setData(LocalDate.now());
+            horario.setEspacosParaReserva(10);
+            horario.setQtdReservados(0);
+            horario.setRestaurante(new Restaurante());
+
+            when(horarioService.buscarPeloId(1L)).thenReturn(horario);
+
+            mockMvc.perform(get("/restaurante/buscarHorarioPeloId/1"))
+                    .andExpect(status().isFound())
+                    .andExpect(jsonPath("$.horaInicio[0]").value(9))
+                    .andExpect(jsonPath("$.horaInicio[1]").value(0));
+
+            verify(horarioService).buscarPeloId(1L);
+        }
+
+        @Test
+        void deveRetornarNotFound_QuandoHorarioNaoExistir() throws Exception {
+            when(horarioService.buscarPeloId(1L))
+                    .thenThrow(new RegistroNotFoundException("Horario", 1L));
+
+            mockMvc.perform(get("/restaurante/buscarHorarioPeloId/1"))
+                    .andExpect(status().isNotFound())
+                    .andExpect(content().string("Horario não encontrado com ID: 1"));
+        }
+
+        @Test
+        void deveAtualizarHorarioComSucesso() throws Exception {
+            fail("Não foi possível implementar devido aos problemas com a biblioteca de time do java 8.");
+
+            Horario horario = new Horario();
+            horario.setIdHorario(1L);
+            horario.setHoraInicio(LocalTime.of(9, 0));
+            horario.setHoraFim(LocalTime.of(17, 0));
+            horario.setData(LocalDate.now());
+            horario.setEspacosParaReserva(10);
+            horario.setQtdReservados(0);
+            horario.setRestaurante(new Restaurante());
+
+            HorarioDTO horarioDTO;
+            horarioDTO = HorarioMapper.toDTO(horario);
+
+
+            when(horarioService.atualizarHorario(eq(1L), any())).thenReturn(horario);
+
+            mockMvc.perform(put("/restaurante/atualizarHorario/1")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(asJsonString(horarioDTO)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.horaInicio[0]").value(9))
+                    .andExpect(jsonPath("$.horaInicio[1]").value(0));
+        }
+
+        @Test
+        void deveDeletarHorarioComSucesso() throws Exception {
+            doNothing().when(horarioService).deletarPeloId(1L);
+
+            mockMvc.perform(delete("/restaurante/deletarHorario/1"))
+                    .andExpect(status().isOk());
+
+            verify(horarioService).deletarPeloId(1L);
+        }
+        @Test
+        void deveAtualizarCapacidadeComSucesso() throws Exception {
+
+            Horario horario = new Horario();
+            horario.setIdHorario(1L);
+            horario.setHoraInicio(LocalTime.of(9, 0));
+            horario.setHoraFim(LocalTime.of(17, 0));
+            horario.setData(LocalDate.now());
+            horario.setEspacosParaReserva(10);
+            horario.setQtdReservados(0);
+            horario.setRestaurante(new Restaurante());
+
+            horario.setEspacosParaReserva(20);
+            when(horarioService.atualizarQuantidadeDeReservasMaximas(1L, 20))
+                    .thenReturn(horario);
+
+            mockMvc.perform(put("/restaurante/atualizarCapacidadeDeReservasNoHorario/1")
+                            .param("capacidade", "20"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.espacosParaReserva").value(20));
+        }
 
     }
 
